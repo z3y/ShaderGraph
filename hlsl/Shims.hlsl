c2943e47df208c1abde4b4aec8fdf9324997b40e
z3y <z3y@protonmail.com>
2022-12-15 18:47:10 +0100

    gradient

--- hlsl/Shims.hlsl
+++ hlsl/Shims.hlsl
@@ -2,14 +2,14 @@
 #define UNITY_MATRIX_I_M   unity_WorldToObject
 #define UNITY_MATRIX_V     unity_MatrixV
 #define UNITY_MATRIX_I_V   unity_MatrixInvV
-#define UNITY_MATRIX_P     OptimizeProjectionMatrix(glstate_matrix_projection)
+// #define UNITY_MATRIX_P     OptimizeProjectionMatrix(glstate_matrix_projection)
 #define UNITY_MATRIX_I_P   ERROR_UNITY_MATRIX_I_P_IS_NOT_DEFINED
 #define UNITY_MATRIX_VP    unity_MatrixVP
 #define UNITY_MATRIX_I_VP  _InvCameraViewProj
-#define UNITY_MATRIX_MV    mul(UNITY_MATRIX_V, UNITY_MATRIX_M)
-#define UNITY_MATRIX_T_MV  transpose(UNITY_MATRIX_MV)
-#define UNITY_MATRIX_IT_MV transpose(mul(UNITY_MATRIX_I_M, UNITY_MATRIX_I_V))
-#define UNITY_MATRIX_MVP   mul(UNITY_MATRIX_VP, UNITY_MATRIX_M)
+// #define UNITY_MATRIX_MV    mul(UNITY_MATRIX_V, UNITY_MATRIX_M)
+// #define UNITY_MATRIX_T_MV  transpose(UNITY_MATRIX_MV)
+// #define UNITY_MATRIX_IT_MV transpose(mul(UNITY_MATRIX_I_M, UNITY_MATRIX_I_V))
+// #define UNITY_MATRIX_MVP   mul(UNITY_MATRIX_VP, UNITY_MATRIX_M)
 
 #if defined(UNITY_INSTANCING_ENABLED)
     #define UNITY_ANY_INSTANCING_ENABLED
@@ -25,6 +25,7 @@ inline float3 Unity_SafeNormalize(float3 inVec)
  
 #include "SpaceTransforms.hlsl"
 #include "StdLib.hlsl"
+#include "Functions.hlsl"
 
 
 
