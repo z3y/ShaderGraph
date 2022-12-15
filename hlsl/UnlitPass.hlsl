8c149d76c6026473db57688a290f71047782aa0b
z3y <z3y@protonmail.com>
2022-12-15 18:28:05 +0100

    yeah

--- hlsl/UnlitPass.hlsl
+++ hlsl/UnlitPass.hlsl
@@ -1,30 +1,27 @@
-// for now
-struct appdata
+PackedVaryings vert(Attributes input)
 {
-    float4 vertex : POSITION;
-    float2 uv : TEXCOORD0;
-};
-
-struct v2f
-{
-    float2 uv : TEXCOORD0;
-    float4 vertex : SV_POSITION;
-};
-
-v2f vert (appdata v)
-{
-    v2f o;
-    o.vertex = UnityObjectToClipPos(v.vertex);
-    //o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-    //UNITY_TRANSFER_FOG(o,o.vertex);
-    return o;
+    Varyings output = (Varyings)0;
+    output = BuildVaryings(input);
+    PackedVaryings packedOutput = PackVaryings(output);
+    return packedOutput;
 }
 
-half4 frag (v2f i) : SV_Target
-{
-    // sample the texture
-//    fixed4 col = tex2D(_MainTex, i.uv);
-SurfaceDescriptionInputs sdi;
-SurfaceDescription sd = SurfaceDescriptionFunction(sdi);
-    return half4(sd.Color, sd.Alpha);
+half4 frag(PackedVaryings packedInput) : SV_TARGET 
+{    
+    Varyings unpacked = UnpackVaryings(packedInput);
+    UNITY_SETUP_INSTANCE_ID(unpacked);
+    UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(unpacked);
+
+    SurfaceDescriptionInputs surfaceDescriptionInputs = BuildSurfaceDescriptionInputs(unpacked);
+    SurfaceDescription surfaceDescription = SurfaceDescriptionFunction(surfaceDescriptionInputs);
+
+// #if _AlphaClip
+//     clip(surfaceDescription.Alpha - surfaceDescription.AlphaClipThreshold);
+// #endif
+
+// #ifdef _ALPHAPREMULTIPLY_ON
+//     surfaceDescription.Color *= surfaceDescription.Alpha;
+// #endif
+
+    return half4(surfaceDescription.Color, surfaceDescription.Alpha);
 }
